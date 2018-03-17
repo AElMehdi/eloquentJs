e0c68e2eba8da850257e2a9ba781eabd6bfde80a
AElMehdi <assirelmehdi@gmail.com>
2018-03-17 09:59:31 +0100

    Add the print function

---
+++ app.js
@@ -0,0 +1,29 @@
+// Check values done
+// Creating the print function
+function print(input){
+  const result = document.getElementById("result").append(input);
+}
+
+
+
+
+
+
+// Chapter5: Functional Programming
+//High Order Function
+//Write a forEach function that takes an array and print it
+//
+// function forEach(elements, action){
+//   for (var i = 0; i < elements.length; i++) {
+//     action(elements[i]);
+//   }
+// }
+//
+// // Has a side effect
+// function print(input){
+//   //  const resultDiv = document.getElementById('result');
+//   console.log(input);
+//
+// }
+//
+// forEach(["name1", "name2", "name3"], print);
