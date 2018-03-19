ca459c5e9c0daef3528f5e871d912c23cd895cea
AElMehdi <assirelmehdi@gmail.com>
2018-03-19 22:01:12 +0100

    Extract paragraphs from emails

--- app.js
+++ app.js
@@ -13,15 +13,53 @@ var cats = {"Spot":true};
   // 'Hobbles the 3rd, Little Iroquois'
 
 // Get pragraphs with born or died
-  const ARCHIVE = ['Emily died 27/04/2006: Black Leclere'
-   + 'born 05/04/2006 (mother Lady Penelope): Red Lion, Doctor Hobbles the 3rd, Little Iroquois',
+  const DUMMY_CONTENT = 'I am going to try putting Fat Igor on his shoulder next time I see\n'
+  .concat('him, very curious what will happen.\n')
+  .concat('Also, the scam I told you about is going better than expected. I have already gotten back\n')
+  .concat('five “payments,” and only one complaint.\n')
+  .concat('It is starting to make me feel a bit bad though. And you are right\n')
+  .concat('that it is probably illegal in some way.\n')
+  .concat('Much love,\n')
+  .concat('Aunt Emily\n');
 
-  'Emily died 22/12/2007: Lady Penelope' +
-  'born 01/01/2008 (mother Nami): Balck Beard, Hoffman, Bruno',
+  const firstEmail = DUMMY_CONTENT
+                                  .concat('died 27/04/2006: Black Leclere\n')
+                                  .concat('born 05/04/2006 (mother Lady Penelope): Red Lion, Doctor Hobbles the 3rd, Little Iroquois');
 
-  'born 05/04/2008 (mother Black Beard): Zoro, Luffy',
+  const secondEmail = DUMMY_CONTENT
+                                  .concat('died 22/12/2007: Lady Penelope\n')
+                                  .concat('born 01/01/2008 (mother Nami): Balck Beard, Hoffman, Bruno');
 
-  'Emily died 06/06/2008: Nami, Zoro'];
+  const thirdEmail = DUMMY_CONTENT
+                                  .concat('died 27/04/2006: Black Leclere\n')
+                                  .concat('born 05/04/2006 (mother Lady Penelope): Red Lion, Doctor Hobbles the 3rd, Little Iroquois');
+
+  const fourthEmail = DUMMY_CONTENT.concat('died 06/06/2008: Nami, Zoro');
+
+
+  const ARCHIVE = [firstEmail, secondEmail, thirdEmail, fourthEmail];
+  // Find pragraphs doing
+  // FInd pragraphs starting with Born or Died
+  // Find names after born, and after died
+  // Add them to the set if born, remove if died
+
+  function findParagraphs(emails){
+    let paragraphs = [];
+    for (var i = 0; i < emails.length; i++) {
+      paragraphs = paragraphs.concat(emails[i].split("\n"));
+    }
+    
+    return paragraphs;
+  }
+
+
+  function forEach(emails, func){
+    for (var i = 0; i < emails.length; i++) {
+      console.log(emails[i]);
+    }
+  }
+
+
 
 
 function print(input){
